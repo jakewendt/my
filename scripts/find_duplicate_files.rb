303b1f785733797ef8bf79e545ad97029cfc3171
Jake <github@jakewendt.com>
2012-07-23 21:59:53 -0700

    Updated file_duplicate_files

--- scripts/find_duplicate_files.rb
+++ scripts/find_duplicate_files.rb
@@ -4,6 +4,7 @@ $*.each do |dir|
 	puts "Processing dir:#{dir}:"
 	Dir["#{dir}/**/*"].each do |file|
 		files[File.basename(file)] ||= []
+#		files[File.basename(file)] << { :path => File.dirname(file) }
 		files[File.basename(file)] << File.dirname(file)
 	end
 end
@@ -12,4 +13,12 @@ duplicates = files.select { |k,v|
 	v.length > 1
 }
 
-puts duplicates.inspect
+duplicates.each do |k,v|
+	puts k
+#	puts v.collect{|v|v[:path]}
+#	puts v
+	v.each do |p|
+		puts p
+		puts `identify -quiet -format "%#" "#{p}/#{k}"`
+	end
+end
